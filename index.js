0d2abd032ab72f012eead91d899a6e42617ca575
Nikhil Sinha <nikhilsinha198@gmail.com>
2023-02-08 10:27:56 +0530

    fixes bug i.e resolving bug around relative link realted

--- index.js
+++ index.js
@@ -11,27 +11,27 @@ const clickListner = (e) => {
 
 	switch (word) {
 		case 'w':
-			let w = new Audio('/sounds//tom-1.mp3');
+			let w = new Audio('sounds/tom-1.mp3');
 			w.play();
 			break;
 		case 'a':
-			let a = new Audio('/sounds//tom-2.mp3');
+			let a = new Audio('sounds/tom-2.mp3');
 			a.play();
 			break;
 		case 's':
-			let s = new Audio('/sounds//tom-3.mp3');
+			let s = new Audio('sounds/tom-3.mp3');
 			s.play();
 			break;
 		case 'd':
-			let d = new Audio('/sounds//tom-4.mp3');
+			let d = new Audio('sounds/tom-4.mp3');
 			d.play();
 			break;
 		case 'j':
-			let j = new Audio('/sounds//crash.mp3');
+			let j = new Audio('sounds/crash.mp3');
 			j.play();
 			break;
 		case 'k':
-			let k = new Audio('/sounds//kick-bass.mp3');
+			let k = new Audio('sounds/kick-bass.mp3');
 			k.play();
 			break;
 		case 'l':
